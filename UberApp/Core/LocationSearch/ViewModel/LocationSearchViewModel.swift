d1f7b58bab2096a43000427b09cbc865f83f2ef8
Alex Sh <giiks131@gmail.com>
2022-12-21 14:43:41 +0100

    added location selection, map annotation and route (beta)

--- UberApp/Core/LocationSearch/ViewModel/LocationSearchViewModel.swift
+++ UberApp/Core/LocationSearch/ViewModel/LocationSearchViewModel.swift
@@ -11,8 +11,9 @@ import MapKit
 class LocationSearchViewModel: NSObject, ObservableObject {
     
     // MARK: — Properties
-    
     @Published var results = [MKLocalSearchCompletion]()
+    @Published var selectedLocationCoordinate: CLLocationCoordinate2D?
+    
     private let searchCompleter = MKLocalSearchCompleter()
     var queryFragment: String = "" {
         didSet {
@@ -20,11 +21,36 @@ class LocationSearchViewModel: NSObject, ObservableObject {
             searchCompleter.queryFragment = queryFragment
         }
     }
-    
+    // MARK: Lifecycle
     override init() {
         super.init()
         searchCompleter.delegate = self
         searchCompleter.queryFragment = queryFragment
+    }
+    
+    // MARK: Helpers
+    
+    func selectLocation(_ localSearch: MKLocalSearchCompletion) {
+        locationSearch(forLocalSearchCompletion: localSearch) { response, error in
+            if let error = error {
+                print("DEBUG: Location search failed with error \(error.localizedDescription)")
+                return
+            }
+            
+            guard let item = response?.mapItems.first else { return }
+            let coordinate = item.placemark.coordinate
+            self.selectedLocationCoordinate = coordinate
+            
+            print("DEBUG: Location coordinate is \(coordinate)")
+        }
+    }
+    
+    func locationSearch(forLocalSearchCompletion localSearch: MKLocalSearchCompletion, completion: @escaping MKLocalSearch.CompletionHandler) {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = localSearch.title.appending(localSearch.subtitle)
+        let search = MKLocalSearch(request: searchRequest)
+        
+        search.start(completionHandler: completion)
     }
 }
 
--- UberApp/Core/LocationSearch/Views/LocationSearchView.swift
+++ UberApp/Core/LocationSearch/Views/LocationSearchView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 
 struct LocationSearchView: View {
     @State private var startLocationText = ""
-    @State private var destinationLocationtext = ""
-    @StateObject var viewModel = LocationSearchViewModel()
+    @Binding var showLocationSearchView: Bool
+    @EnvironmentObject var viewModel: LocationSearchViewModel
     
     var body: some View {
         VStack {
@@ -55,6 +55,10 @@ struct LocationSearchView: View {
                     ForEach(viewModel.results, id: \.self) { result in
                         
                         LocationSearchResultsCell(title: result.title, subTitle: result.subtitle)
+                            .onTapGesture {
+                                viewModel.selectLocation(result)
+                                showLocationSearchView.toggle()
+                            }
                     }
                 }
             }
@@ -65,6 +69,6 @@ struct LocationSearchView: View {
 
 struct LocationSearchView_Previews: PreviewProvider {
     static var previews: some View {
-        LocationSearchView()
+        LocationSearchView(showLocationSearchView: .constant(false))
     }
 }
